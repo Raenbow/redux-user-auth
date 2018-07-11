827a7313822994c8ccb0ec0c56da3e10f794d0ea
delray <d.a.faskey@gmail.com>
2018-07-11 15:36:29 -0700

    Added routes and nav bar

---
+++ src/components/home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default props => {
+    return (
+        <div>
+            <h1 className="text-center">
+                Welcome to Movie Quotes!
+            </h1>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente, nostrum illum consequatur aspernatur aliquam natus cum magni harum vitae distinctio id eos dolorum, quas iste quis ad non obcaecati saepe.
+                Natus debitis rerum at repudiandae quaerat a, illum beatae magni illo aut tempore expedita temporibus? Repudiandae quas odit sit enim, ratione quis perferendis aliquam rerum culpa explicabo eligendi inventore laborum.
+                Cum vel vitae accusamus tempora aspernatur maiores voluptates dolores? Voluptatibus, eveniet sed recusandae corrupti facere veritatis neque dicta reiciendis molestiae, id accusantium adipisci sequi? Ut quibusdam sunt quas placeat accusantium!
+                Saepe facilis explicabo velit iste! Consequatur iste quas itaque iure nesciunt assumenda quae rem temporibus earum possimus eveniet voluptate neque impedit iusto eaque laboriosam deserunt pariatur fugiat, at perferendis! Hic.
+            </p>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error quasi beatae facilis laudantium, similique molestias voluptas tempora. Facilis natus sapiente velit? Sunt vero nam corrupti, expedita suscipit quos dolores corporis!Quidem laudantium dolor accusantium, quibusdam laborum aspernatur aperiam assumenda minus quia, vitae ullam deserunt sint. Iure numquam dolore ex repellat obcaecati vel expedita id, aspernatur cum nobis culpa in doloribus!
+            </p>
+        </div>
+    )
+}
